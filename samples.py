e7cfc57db8528cd0f4372e6a8a13033c4d1af7f2
pavel.kaparykha <pavel.kaparykha@spglobal.com>
2023-10-22 12:10:15 +0200

    added replace pattern

--- samples.py
+++ samples.py
@@ -4,3 +4,13 @@ def get_template():
         template_lines = template_file.readlines()
         print(template_lines)
         return template_lines
+
+
+
+def get_greeting(my_name, my_age):
+    my_str_template = 'My name is {name}. I\'m {number} years old.'
+    str2 = my_str_template.replace('{name}', my_name)
+    str2 = str2.replace('{number}', my_age)
+    return str2
+
+
